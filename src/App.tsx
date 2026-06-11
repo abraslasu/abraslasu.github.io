2b66963c26aa072b35f29f57c16bafc5c351edc0
Alexandra Braslasu <abraslasu@gmail.com>
2026-06-11 20:09:58 +0300

    update basename

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import DetailPage from './pages/DetailPage';
 
 function App() {
   return (
-    <BrowserRouter basename="/abraslasu.gihtub.io">
+    <BrowserRouter basename="/">
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
